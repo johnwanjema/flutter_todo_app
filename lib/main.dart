41b75da2844ede31274b081f0ba9dab227627c2d
john <jonwanjema@gmail.com>
2020-11-24 13:30:30 +0300

    add homepage widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily:'avenir',
         primarySwatch: Colors.blue,
       ),
-      home:Text('Todo app'),
+      home: MyHomePage(),
     );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
 
