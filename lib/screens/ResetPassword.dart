5628462a0b0a7e9d6e013bb5616b849052642f21
john <jonwanjema@gmail.com>
2020-12-01 11:46:17 +0300

    add body for reset password page

--- lib/screens/ResetPassword.dart
+++ lib/screens/ResetPassword.dart
@@ -3,6 +3,108 @@ import 'package:flutter/material.dart';
 class ResetPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Avenir'),
+      debugShowCheckedModeBanner: false,
+      home: resetPassword(),
+    );
+  }
+}
+
+class resetPassword extends StatefulWidget {
+  @override
+  _resetPasswordState createState() => _resetPasswordState();
+}
+
+class _resetPasswordState extends State<resetPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Reset Password',
+              style: TextStyle(fontSize: 35),
+            ),
+            Text(
+              'Reset code will be sent to your email. Please enter the code and create a new password.',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Code',
+              style: TextStyle(
+                fontSize: 23,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: '****'),
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Password',
+              style: TextStyle(
+                fontSize: 23,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'Enter your new password'),
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Confirm Password',
+              style: TextStyle(
+                fontSize: 23,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'Confirm your new password'),
+              style: TextStyle(fontSize: 20),
+            ),
+            Expanded(
+              child: Center(
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(1000),
+                        // borderRadius: BorderRadius.all(
+                        //   Radius.circular(7),
+                        // ),
+                        color: Colors.red),
+                    child: Text(
+                      'Reset Password',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
