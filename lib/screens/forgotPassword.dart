dde45e25866a0e489e6a7acc144324516e5e006a
john <jonwanjema@gmail.com>
2020-12-01 11:25:47 +0300

    add appbar body

--- lib/screens/forgotPassword.dart
+++ lib/screens/forgotPassword.dart
@@ -3,6 +3,81 @@ import 'package:flutter/material.dart';
 class ForgotPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: forgotPassword(),
+      theme: ThemeData(fontFamily: 'Avenir'),
+    );
+  }
+}
+
+class forgotPassword extends StatefulWidget {
+  @override
+  _forgotPasswordState createState() => _forgotPasswordState();
+}
+
+class _forgotPasswordState extends State<forgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent ,
+        leading: IconButton(icon: Icon(Icons.arrow_back),),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Forgot Password',
+              style: TextStyle(fontSize: 35),
+            ),
+            Text(
+              'Please enter your email address to reset password',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Email',
+              style: TextStyle(
+                fontSize: 23,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'jondoe@gmail.com'),
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Expanded(
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(1000),
+                      // borderRadius: BorderRadius.all(
+                      //   Radius.circular(7),
+                      // ),
+                      color: Colors.red
+                  ),
+                  child: Text('Reset Password',style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white
+                  ),),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
