a1996840f5df9c752ec14672cbbe85fe950f17a0
john <jonwanjema@gmail.com>
2020-12-10 18:13:34 +0300

    add appbar for  task screen

--- lib/screens/NewTask.dart
+++ lib/screens/NewTask.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 
+import 'HomeScreen.dart';
+
 class NewTask extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: newTask(),
+      theme: ThemeData(fontFamily: 'avenir'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class newTask extends StatefulWidget {
+  @override
+  _newTaskState createState() => _newTaskState();
+}
+
+class _newTaskState extends State<newTask> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        elevation: 0,
+        title: Text(
+          'New Task',
+          style: TextStyle(fontSize: 25),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed:  (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+          },
+        ),
+      ),
+      // body: ,
+    );
   }
 }
