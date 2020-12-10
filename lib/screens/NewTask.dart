ef7f5aba6763b95f6d12d6ebdcef04e82062d333
john <jonwanjema@gmail.com>
2020-12-10 19:19:29 +0300

    add task page details

--- lib/screens/NewTask.dart
+++ lib/screens/NewTask.dart
@@ -21,6 +21,7 @@ class newTask extends StatefulWidget {
 class _newTaskState extends State<newTask> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -35,12 +36,215 @@ class _newTaskState extends State<newTask> {
             Icons.arrow_back,
             color: Colors.white,
           ),
-          onPressed:  (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomePage()));
           },
         ),
       ),
-      // body: ,
+      body: Container(
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              height: 30,
+              color: Color(0xfff96060),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                height: 70,
+                width: size.width,
+                color: Colors.black.withOpacity(.8),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(7)),
+                color: Colors.white,
+              ),
+              width: size.width,
+              height: size.height * .85,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          'For',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
+                              ),
+                              color: Colors.grey.withOpacity(.2)),
+                          child: Text(
+                            'Assignee',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'In',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
+                              ),
+                              color: Colors.grey.withOpacity(.2)),
+                          child: Text(
+                            'Project',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      color: Colors.grey.withOpacity(.2),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            hintText: "Title", border: InputBorder.none),
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Description',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: 150,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(15),
+                                  topLeft: Radius.circular(15),
+                                ),
+                                border: Border.all(
+                                    color: Colors.grey.withOpacity(.5))),
+                            child: TextField(
+                              maxLines: 6,
+                              decoration: InputDecoration(
+                                  hintText: 'Add Description',
+                                  border: InputBorder.none),
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Container(
+                            height: 50,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                                color: Colors.grey.withOpacity(.2),
+                                borderRadius: BorderRadius.only(
+                                    bottomRight: Radius.circular(15),
+                                    bottomLeft: Radius.circular(15)),
+                                border: Border.all(
+                                    color: Colors.grey.withOpacity(.5))),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Container(
+                                  child: IconButton(
+                                    icon: Icon(
+                                      Icons.attach_file,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            color: Colors.grey.withOpacity(.2),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Due Date",
+                                  border: InputBorder.none),
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Add Member',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20),
+                                ),
+                                color: Colors.grey.withOpacity(.2)),
+                            child: Text(
+                              'Anyone',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 15),
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15),
+                              ),
+                              color:  Color(0xfff96060),
+                            ),child: Center(
+                            child: Text('Add Task',style: TextStyle(
+                              fontSize: 18
+                            ),),
+                          ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
