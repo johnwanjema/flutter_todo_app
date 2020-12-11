a8fe28e3c782537e4df81c4d747c4db165348af5
john <jonwanjema@gmail.com>
2020-12-11 07:04:42 +0300

    add app bar for checklist page

--- lib/screens/NewCheckList.dart
+++ lib/screens/NewCheckList.dart
@@ -1,8 +1,47 @@
 import 'package:flutter/material.dart';
 
+import 'HomeScreen.dart';
+
 class NewCheckList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'avenir'
+      ),
+      home: newCheckList(),
+    );
+  }
+}
+
+class newCheckList extends StatefulWidget {
+  @override
+  _newCheckListState createState() => _newCheckListState();
+}
+
+class _newCheckListState extends State<newCheckList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        elevation: 0,
+        title: Text(
+          'New Checklist',
+          style: TextStyle(fontSize: 25),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomePage()));
+          },
+        ),
+      ),
+    );
   }
 }
