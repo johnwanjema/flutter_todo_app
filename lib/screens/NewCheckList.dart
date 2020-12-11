73a37ce1a3406509810f95dfdbd55dccdb7477bd
john <jonwanjema@gmail.com>
2020-12-11 07:15:37 +0300

    add checklist widget

--- lib/screens/NewCheckList.dart
+++ lib/screens/NewCheckList.dart
@@ -21,8 +21,10 @@ class newCheckList extends StatefulWidget {
 }
 
 class _newCheckListState extends State<newCheckList> {
-  @override
+  List<int>  selectedList = [];
+   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
@@ -40,6 +42,153 @@ class _newCheckListState extends State<newCheckList> {
             Navigator.push(
                 context, MaterialPageRoute(builder: (context) => HomePage()));
           },
+        ),
+      ),
+      body: Container(
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              height: 30,
+              color: Color(0xfff96060),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                height: 70,
+                width: size.width,
+                color: Colors.black.withOpacity(.8),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(7)),
+                color: Colors.white,
+              ),
+              width: size.width,
+              height: size.height * .85,
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Title',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'Lorem ispium',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            CheckboxListTile(
+                              title: Text('List item 1'),controlAffinity: ListTileControlAffinity.leading,
+                              onChanged: (bool value){
+                                setState(() {
+                                  if(value){
+                                    selectedList.add()
+                                  }else{
+                                    selectedList.remove()
+                                  }
+                                });
+                              },
+                              value: selectedList.container(i),
+                            ),
+                            Text(
+                              'Colour',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.blue,
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10),
+                                      )),
+                                ),
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.pink,
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10),
+                                      )),
+                                ),
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10),
+                                      )),
+                                ),
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.orange,
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10),
+                                      )),
+                                ),
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.deepPurple,
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10),
+                                      )),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(vertical: 15),
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                                color: Color(0xfff96060),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Add CheckList',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
