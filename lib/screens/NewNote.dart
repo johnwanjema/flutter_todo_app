2b386d46cb968becae220fa55a139e217886e5a1
john <jonwanjema@gmail.com>
2020-12-11 06:48:58 +0300

    add appbar for notes screen

--- lib/screens/NewNote.dart
+++ lib/screens/NewNote.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
 
+import 'HomeScreen.dart';
+
 class NewNote extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: newNote(),
+      theme: ThemeData(fontFamily: 'avenir'),
+    );
+  }
+}
+
+class newNote extends StatefulWidget {
+  @override
+  _newNoteState createState() => _newNoteState();
+}
+
+class _newNoteState extends State<newNote> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        elevation: 0,
+        title: Text(
+          'New Note',
+          style: TextStyle(fontSize: 25),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomePage()));
+          },
+        ),
+      ),body: ,
+    );
   }
 }
